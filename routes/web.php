3fe20c1ac96e8fc921dae28bcddbfd6666c7a0ca
Kaye Catigay <kayer1544@gmail.com>
2023-03-01 16:06:34 +0800

    login base

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index']);
 
+Route::get('/login', [App\Http\Controllers\Auth\LoginController::class, 'login']);
 
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
